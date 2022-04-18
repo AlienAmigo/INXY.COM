4a0ee1d1013a54041585ad1cd682085529d7f8a5
Pyotr Ryzhov <maestro_pete@inbox.ru>
2022-04-18 19:25:47 +0300

    styled addaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,36 @@
 function ready(fn) {
-  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+  if (
+    document.attachEvent
+      ? document.readyState === 'complete'
+      : document.readyState !== 'loading'
+  ) {
     fn();
   } else {
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
 
-ready(function(){
+ready(function () {
   const SortBtn = document.querySelector('#sort-btn');
   if (SortBtn) {
     SortBtn.addEventListener('click', () => {
       SortBtn.classList.toggle('active');
     });
+  }
+
+  const SearchTrigger = document.querySelector('#search-trigger');
+  const SearchForm = document.querySelector('#search-form');
+  const ToggleForm = () => {
+    SearchForm.classList.toggle('active');
+    SearchTrigger.classList.toggle('disable');
+  };
+
+  if (SearchTrigger && SearchForm) {
+    SearchForm.addEventListener('submit', ev => {
+      ev.preventDefault();
+      ToggleForm();
+    });
+
+    SearchTrigger.addEventListener('click', () => ToggleForm());
   }
 });
